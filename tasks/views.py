ee0b17a6b5c402de189a5afcaf102408ec8b5fce
Mai4485 <maiza4485@gmail.com>
2025-09-05 01:31:40 +0700

    eiei

---
+++ tasks/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+
+def index(request):
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'tasks/index.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        Task.objects.create(title=title)
+    return redirect('index')
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('index')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('index')
